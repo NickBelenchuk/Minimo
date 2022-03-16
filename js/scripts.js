76f06a060f7e0079e85a783436dce54a95333408
Klaus Belenchuk <nick.belenchuk@gmail.com>
2022-03-16 15:34:38 +0200

    init

---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+// Custom Scripts
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+  const body = document.querySelector('body');
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active');
+      burger.classList.add('active-burger');
+      body.classList.add('locked');
+    } else {
+      menu.classList.remove('active');
+      burger.classList.remove('active-burger');
+      body.classList.remove('locked');
+    }
+  });
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768.98) {
+      menu.classList.remove('active');
+      burger.classList.remove('active-burger');
+      body.classList.remove('locked');
+    }
+  });
+}
+burgerMenu();
+
+// ***************SWIPER****************
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+  const nav = document.querySelector('nav');
+
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+  const breakpoint = 1;
+  if (window.scrollY >= breakpoint) {
+    nav.classList.add('fixed__nav');
+  } else {
+    nav.classList.remove('fixed__nav');
+  }
+}
+window.addEventListener('scroll', fixedNav);
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+  showSlides((slideIndex += 1));
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+  showSlides((slideIndex -= 1));
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('item');
+  var dots = document.getElementsByClassName('slider-dots_item');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
+
+
